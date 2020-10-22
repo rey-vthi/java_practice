caa65e44c88c567d18990237fb66618843c15718
rey-vthi <108cs16047@gmail.com>
2020-10-22 16:47:31 +0530

    si

--- Day1/SimpleInterest.java
+++ Day1/SimpleInterest.java
@@ -1,10 +1,10 @@
 public class SimpleInterest{
   public static void main(String[] args) {
-    int principle = Integer.parseInt(args[0]);
-    int rateOfInterest = Integer.parseInt(args[1]);
-    int time = Integer.parseInt(args[2]);
-
-    double si = (principle * time * rateOfInterest)/100.0;
-    System.out.println("Simple interest is "+si);
+    int principal = Integer.parseInt(args[0]);
+    int rate = Integer.parseInt(args[1]);
+    int period = Integer.parseInt(args[2]);
+    
+    double simpleInterest=(principal*rate*period)/100;
+    System.out.println(simpleInterest);
   }
 }
