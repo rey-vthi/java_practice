321333c7e6c5de6d83e5a54a86785512defcfba7
rey-vthi <108cs16047@gmail.com>
2020-10-22 16:46:51 +0530

    modified ci

--- Day1/CompoundInterest.java
+++ Day1/CompoundInterest.java
@@ -1,17 +1,11 @@
 public class CompoundInterest{
   public static void main(String[] args) {
-    int p = Integer.parseInt(args[0]);
-    double r = Integer.parseInt(args[1])/100.0;
-    int t = Integer.parseInt(args[2]);
+    int principal = Integer.parseInt(args[0]);
+    int rate = Integer.parseInt(args[1]);
+    int period = Integer.parseInt(args[2]);
+
+    double compoundInterest = ((principal *( (1 + rate) ** period)) - principal);
     
-    double a = p;
-    double ci;
-
-    for(int i=t; t>0; t--) {
-      a = a *(1+r);
-    }
-
-    ci = a-p;
-    System.out.println("Compound Interest is " + ci);
+    System.out.println(compoundInterest);
   }
 }
