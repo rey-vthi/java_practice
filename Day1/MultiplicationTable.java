69e72aba353595f51e639f98f39680895a3b0546
rey-vthi <108cs16047@gmail.com>
2020-10-22 16:39:56 +0530

    removed

--- Day1/MultiplicationTable.java
+++ Day1/MultiplicationTable.java
@@ -3,10 +3,8 @@ public class MultiplicationTable {
     int m = Integer.parseInt(args[0]);
     int n = Integer.parseInt(args[1]);
 
-    int product;
     for(int i = 1; i <= n; i++ ) {
-      product = m * i;
-      System.out.println(m + " x " + i + " = " + product);
+      System.out.println(m + " x " + i + " = " + m * i);
     }
   }
 }
