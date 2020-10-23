228deff145e7232b28475b0c6cc71b1c3e9e1e25
rey-vthi <108cs16047@gmail.com>
2020-10-23 13:29:28 +0530

    added greater and greatest method

--- Day1/GreatestOfThreeNumbers.java
+++ Day1/GreatestOfThreeNumbers.java
@@ -1,13 +1,25 @@
 public class GreatestOfThreeNumbers{
+
+  public static int greater(int num1, int num2) {
+    return num1 > num2 ? num1: num2;
+  }
+
+  public static int greatest(int num1, int num2, int num3) {
+    return greater(greater(num1, num2), num3);
+  }
+
   public static void main(String[] args) {
+
+    if(args.length != 3) {
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
     int num1 = Integer.parseInt(args[0]);
     int num2 = Integer.parseInt(args[1]);
     int num3 = Integer.parseInt(args[2]);
 
-    int greatest = num1 > num2? num1:num2;
-    greatest = greatest>num3?greatest:num3;
-
-    System.out.println("Greatest among them is " + greatest);
+    System.out.println("Greatest among them is " + greatest(num1, num2, num3));
     
   }
 }
