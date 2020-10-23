0eca82e50bf76ea8cfa2c6ab7127b5e41f859889
rey-vthi <108cs16047@gmail.com>
2020-10-23 12:35:45 +0530

    pulled convertToFahrenheit method in class

--- Day1/CentigradeToFahrenheit.java
+++ Day1/CentigradeToFahrenheit.java
@@ -1,8 +1,12 @@
 public class CentigradeToFahrenheit{
+
+  public static double convertToFahrenheit(int centigrade){
+    return ((centigrade * 9) / 5.0) + 32;
+  }
+
   public static void main(String[] args) {
     int centigrade = Integer.parseInt(args[0]);
-    double fahrenheit = ((centigrade * 9) / 5.0) + 32;
 
-    System.out.println(centigrade + "c is equal to " + fahrenheit + "f");
+    System.out.println(centigrade + "c is equal to " + convertToFahrenheit(centigrade) + "f");
   }
 }
