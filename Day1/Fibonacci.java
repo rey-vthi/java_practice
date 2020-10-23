8384b2b836b956bcb822d3b140898d38936ea348
rey-vthi <108cs16047@gmail.com>
2020-10-23 13:34:27 +0530

    added method to print fibonacci series

--- Day1/Fibonacci.java
+++ Day1/Fibonacci.java
@@ -1,13 +1,24 @@
 public class Fibonacci {
-  public static void main(String[] args) {
-  int count = Integer.parseInt(args[0]);
-    int currentNo = 0;
+
+  public static void printFibonacciSeries(int numOfTerms) {
+    int currentNum = 0;
     int nextNum = 1;
 
-    for(int i = 1; i <= count; i++){
-      System.out.println(currentNo);
-      nextNum = currentNo + nextNum;;
-      currentNo = nextNum - currentNo;
+    for(int term = 1; term <= numOfTerms; term++){
+      System.out.println(currentNum);
+      nextNum = currentNum + nextNum;;
+      currentNum = nextNum - currentNum;
     }
+  }
+  
+  public static void main(String[] args) {
+    if(args.length != 1) {
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
+    int numOfTerms = Integer.parseInt(args[0]);
+
+    printFibonacciSeries(numOfTerms);
   }
 }
