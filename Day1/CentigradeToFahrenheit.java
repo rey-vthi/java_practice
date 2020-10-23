75700d46c32cd39678e53d62b603fef7a8062b3b
rey-vthi <108cs16047@gmail.com>
2020-10-23 12:38:47 +0530

    checked args length and modified gitignore

--- Day1/CentigradeToFahrenheit.java
+++ Day1/CentigradeToFahrenheit.java
@@ -1,10 +1,16 @@
-public class CentigradeToFahrenheit{
+public class CentigradeToFahrenheit {
 
-  public static double convertToFahrenheit(int centigrade){
+  public static double convertToFahrenheit(int centigrade) {
     return ((centigrade * 9) / 5.0) + 32;
   }
 
   public static void main(String[] args) {
+
+    if(args.length != 1) {
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
     int centigrade = Integer.parseInt(args[0]);
 
     System.out.println(centigrade + "c is equal to " + convertToFahrenheit(centigrade) + "f");
