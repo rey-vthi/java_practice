903ed2cfcc092627c8796a0ef33096754a8ff3b1
rey-vthi <108cs16047@gmail.com>
2020-10-23 13:11:49 +0530

    added printMultiplicationTable and checked the args count

--- Day1/MultiplicationTable.java
+++ Day1/MultiplicationTable.java
@@ -1,10 +1,21 @@
 public class MultiplicationTable {
-  public static void main(String[] args) {
-    int m = Integer.parseInt(args[0]);
-    int n = Integer.parseInt(args[1]);
 
+  public static void printMultiplicationTable(int m, int n) {
     for(int i = 1; i <= n; i++ ) {
       System.out.println(m + " x " + i + " = " + (m * i));
     }
+  }
+
+  public static void main(String[] args) {
+
+    if(args.length != 2) {
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
+    int m = Integer.parseInt(args[0]);
+    int n = Integer.parseInt(args[1]);
+
+    printMultiplicationTable(m,n);
   }
 }
