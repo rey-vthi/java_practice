a5d1d761b7499f788ba614671f648fde05c3219a
rey-vthi <108cs16047@gmail.com>
2020-10-23 12:43:39 +0530

    added convertToCentigrade method and checked arguments lenght

--- Day1/FahrenheitToCentigrade.java
+++ Day1/FahrenheitToCentigrade.java
@@ -1,8 +1,17 @@
 public class FahrenheitToCentigrade {
-  public static void main(String[] args) {
-    int fahrenheit = Integer.parseInt(args[0]);
-    double centigrade = ((fahrenheit-32) * 5) / 9.0;
 
-    System.out.println(fahrenheit + "f is equal to " + centigrade + "c");
+  public static double convertToCentigrade(int fahrenheit) {
+    return ((fahrenheit - 32) * 5) / 9.0;
+  }
+
+  public static void main(String[] args) {
+    if(args.length != 1) {
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
+    int fahrenheit = Integer.parseInt(args[0]);
+
+    System.out.println(fahrenheit + "f is equal to " + convertToCentigrade(fahrenheit) + "c");
   }
 }
