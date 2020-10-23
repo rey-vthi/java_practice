c5f1c124fff6a5c72390534d88215c7adac72efe
rey-vthi <108cs16047@gmail.com>
2020-10-23 13:06:38 +0530

    added getSimpleInterest method and checked the number of arguments

--- Day1/SimpleInterest.java
+++ Day1/SimpleInterest.java
@@ -1,11 +1,20 @@
 public class SimpleInterest{
+
+  public static double getSimpleInterest(int principal, int rate, int period) {
+    return (principal * rate * period) / 100;
+  }
+
   public static void main(String[] args) {
+
+    if(args.length != 3) {
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
     int principal = Integer.parseInt(args[0]);
     int rate = Integer.parseInt(args[1]);
     int period = Integer.parseInt(args[2]);
     
-    double simpleInterest=(principal*rate*period)/100;
-    
-    System.out.println(simpleInterest);
+    System.out.println("Simple Interest is " + getSimpleInterest(principal,rate,period));
   }
 }
